11caf3037f8a0fea83893bc9c3e50e04cdbb6577
AnapartiNandini <43045158+AnapartiNandini@users.noreply.github.com>
2019-01-14 15:27:22 -0600

    Final Project in Software Development
    This is my final project in my software development class. In order to open it you will need to have the Greenfoot software.

---
+++ Flappy Bird - Final Project (Greenfoot)/FlappyBird.java
@@ -0,0 +1,28 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class FlappyBird here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class FlappyBird extends Actor
+{
+    //Instance Variables that store the possible images for the Flappy Bird
+    private GreenfootImage blueFlappyBird = new GreenfootImage(" Flappy_image_1_BLUE.png");
+    private GreenfootImage greenFlappyBird = new GreenfootImage("Flappy_image_1_GREEN.png");
+    private GreenfootImage redFlappyBird = new GreenfootImage("Flappy_image_1_RED.png");
+    private GreenfootImage yellowFlappyBird = new GreenfootImage("Flappy_image_1_YELLOW");
+    private GreenfootImage blueFlappyBird2 = new GreenfootImage(" Flappy_image_2_BLUE.png");
+    private GreenfootImage greenFlappyBird2 = new GreenfootImage("Flappy_image_2_GREEN.png");
+    private GreenfootImage redFlappyBird2 = new GreenfootImage("Flappy_image_2_RED.png");
+    private GreenfootImage yellowFlappyBird2 = new GreenfootImage("Flappy_image_2_YELLOW");
+    /**
+     * Act - do whatever the FlappyBird wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        
+    }    
+}
---
+++ Flappy Bird - Final Project (Greenfoot)/MyWorld.java
@@ -0,0 +1,59 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends greenfoot.World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(800, 600, 1);
+        
+        //Make a method call to prepareWorld
+        prepareWorld();
+        
+        //Make a method call to addActors
+        addActors();
+    }
+    
+    /**
+     * prepareWorld prepares the world as in sets up the back-drop
+     * 
+     * @param None There are no parameters
+     * @return Nothing is returned 
+     */
+    private void prepareWorld()
+    {
+        //sets the color of the background to cyan
+        getBackground().setColor(Color.CYAN);
+        
+        //draws a rectangle at (0,0) at a width of the window's width and a height of the window's height minus a quatre
+        getBackground().fillRect(0,0,getWidth(),getHeight()-getHeight()/4);
+        
+        //sets the color of the ground to be green
+        getBackground().setColor(Color.GREEN);
+        
+        //draws a rectangle at (0,getHeight()-getHeight()/4, getWidth(), getHeight())
+        getBackground().fillRect(0,getHeight()-getHeight()/4, getWidth(), getHeight());
+        
+        //sets the color of the "horizon" to orange
+        getBackground().setColor(Color.ORANGE);
+        
+        //draws a rectangle at (0,getHeight() - getHeight()/4 - 5, getWidth(), 10)
+        getBackground().fillRect(0,getHeight() - getHeight()/4 - 5, getWidth(), 10);
+    }
+    
+    private void addActors()
+    {
+        addObject(new FlappyBird (), getWidth()/4, getHeight()/2);
+    }
+}
